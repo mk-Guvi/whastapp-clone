575f965eae81eb66e11937562180e4b1b48cf00b
mk-Guvi <61502453+mk-Guvi@users.noreply.github.com>
2020-12-23 16:29:55 +0530

    first commit untill added css to app andapp__body class(5)

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBhbjx7lo2lISw_iCQ9X5qBFbLEs3arIjw",
+  authDomain: "whatsapp-clone-e7944.firebaseapp.com",
+  projectId: "whatsapp-clone-e7944",
+  storageBucket: "whatsapp-clone-e7944.appspot.com",
+  messagingSenderId: "1066135555187",
+  appId: "1:1066135555187:web:f61d4b9c10da9089202e06",
+  measurementId: "G-CYR9BZ8SQ5"
+};
